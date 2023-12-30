e907accc87a6cd132f7cd58ce0353c555e1eb678
shasha333333 <63197886+shasha333333@users.noreply.github.com>
2023-12-30 20:59:35 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShoeSalesSystem.Helpers;
+using ShoeSalesSystem.Forms;
+
+
+namespace ShoeSalesSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+            //Application.Run(new Form1());
+            //SqlHelper.Test();
+        }
+    }
+}
